6f641dd52f3cbb031ad5f7561edec409d519d788
Christiaan van Harten <christiaanvh@gmail.com>
2023-12-22 20:46:33 +0100

    da18b

--- src/java/chris/Day18.java
+++ src/java/chris/Day18.java
@@ -92,79 +92,77 @@ public class Day18 {
     private static String puzzel2() {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("puzzel18.txt"))) {
 
-            List<Pos> fill = new ArrayList<>();
             String regel;
             int x = 0;
             int y = 0;
             while ((regel = bufferedReader.readLine()) != null) {
                 String[] parts = regel.split(" ");
-                String lengthS = parts[2].substring(2,7);
-                String direction =  parts[2].substring(7,8);
-                Long length = Long.parseLong(lengthS, 16);
-                for (int i = 0; i < length; i++) {
-                    trenches.add(new Trench(x, y, parts[2].substring(2, parts[2].length() - 1)));
-                    switch (direction) {
-                        case "0" -> x++;
-                        case "1" -> y++;
-                        case "2" -> x--;
-                        case "3" -> y--;
-                    }
-                }
+
+                    trenches.add(new Trench(0, 0, parts[2]));
+
             }
-            List<List<Character>> fields = new ArrayList<>();
-            int minX = trenches.stream().min((a, b) -> a.x - b.x).get().x;
-            int minY = trenches.stream().min((a, b) -> a.y - b.y).get().y;
-            int maxX = trenches.stream().max((a, b) -> a.x - b.x).get().x + 1;
-            int maxY = trenches.stream().max((a, b) -> a.y - b.y).get().y + 1;
-            for(int y1 = minY; y1<= maxY; y1++) {
-                List<Character> row = new ArrayList<>();
-                for(int x1 = minX; x1  <= maxX; x1++ ) {
-                    boolean match = false;
-                    for (int t = 0; t < trenches.size(); t++) {
-                        if (trenches.get(t).x == x1 && trenches.get(t).y == y1) {
-                            row.add('#');
-                            match = true;
-                        }
-                    }
-                    if(!match) row.add('.');
-                }
-                fields.add(row);
-            }
+            List<Pos> hoeken = routeGraven(trenches);
 
-            fields.forEach(col -> {
-                col.forEach(cell -> System.out.print(cell));
-                System.out.println();
-            });
-
-            //fields.get(87).set(18, '*');
-            long it = 0L;
-            while(true) {
-                it++;
-                boolean chamged = false;
-                for(int yy=0; yy< fields.size();yy++) {
-                    for (int xx = 0; xx< fields.get(0).size(); xx++) {
-                        if(fields.get(yy).get(xx) == '*') {
-                            if (yy > 0 && fields.get(yy -1).get(xx) != '#' && fields.get(yy -1).get(xx) != '*') {fields.get(yy-1).set(xx, '*'); chamged = true;}
-                            if (yy < fields.size()-1 && fields.get(yy +1).get(xx) != '#' && fields.get(yy +1).get(xx) != '*') {fields.get(yy+1).set(xx, '*'); chamged = true;}
-                            if (xx > 0 && fields.get(yy).get(xx-1) != '#' && fields.get(yy).get(xx-1) != '*') {fields.get(yy).set(xx -1, '*'); chamged = true;}
-                            if (xx < fields.get(yy).size()-1 && fields.get(yy).get(xx + 1) != '#' && fields.get(yy).get(xx + 1) != '*') {fields.get(yy).set(xx + 1, '*'); chamged =true;}
-                        }
-
-                    }
-                }
-                if(!chamged) break;
-            }
-            AtomicInteger aantal = new AtomicInteger();
-            fields.forEach(charList -> {
-                charList.forEach(chr -> {
-                    if(chr=='#' || chr=='*') aantal.incrementAndGet();
-                });
-            });
-
-            return aantal.toString();
+            return "" +(getOppervlakte(hoeken) + getAfstand(hoeken));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Pos volgendePositie(Pos huidig, int richting, long lengte) {
+        switch (richting) {
+            case 0:
+                return new Pos(huidig.x + lengte, huidig.y);
+            case 2:
+                return new Pos(huidig.x - lengte, huidig.y);
+            case 3:
+                return new Pos(huidig.x, huidig.y - lengte);
+            case 1:
+            default:
+                return new Pos(huidig.x, huidig.y + lengte);
+        }
+
+    }
+
+    private static List<Pos> routeGraven(List<Trench> trenches) {
+        List<Pos> hoeken = new ArrayList<>();
+        Pos huidig = new Pos(0, 0);
+        hoeken.add(huidig);
+        for (int i = 0; i < trenches.size(); i++) {
+            Long afstand = Long.parseLong(trenches.get(i).color.substring(2, 7),16);
+            int richting = Integer.parseInt(trenches.get(i).color.substring(7,8));
+            huidig = volgendePositie(huidig, richting, afstand);
+            if (i != trenches.size() - 1) {
+                hoeken.add(huidig);
+            }
+        }
+        return hoeken;
+    }
+
+    private static long getAfstand(List<Pos> hoeken) {
+        long total = 0;
+
+        for (int i = 0; i < hoeken.size(); i++) {
+            Pos c1 = hoeken.get(i);
+            Pos c2 = hoeken.get(i == hoeken.size() - 1 ? 0 : i + 1);
+            total += Math.abs(c1.x - c2.x) + Math.abs(c1.y - c2.y);
+        }
+
+        return total;
+    }
+
+    private static long getOppervlakte(List<Pos> hoeken) {
+        // Start with shoelace
+        long area = 0;
+        for (int i = 0; i < hoeken.size(); i++) {
+            Pos c1 = hoeken.get(i);
+            Pos c2 = hoeken.get(i == hoeken.size() - 1 ? 0 : i + 1);
+            long factor = c1.x * c2.y - c1.y * c2.x;
+            area += factor;
+        }
+        area /= 2;
+        // Now apply Pick's theorem
+        return area - (getAfstand(hoeken) / 2) + 1;
     }
 
     private static class Trench {
